a4257bcf308bd392be825edb3ca8859a5ffea50e
Arsalan Mustafa <arsalanmustafak@gmail.com>
2023-12-05 07:20:59 +0530

    editing main.py

--- service/main.py
+++ service/main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
-import onnxruntime as rt
 from service.api.api import main_router
 
 app = FastAPI(project_name= "Emotion Detection")
 app.include_router(main_router)
-
-model_path = 'service/vit_onnx.onnx'
-providers = ['CPUExecutionProvider']
-m_q = rt.InferenceSession(model_path, providers = providers)
 
 @app.get('/')
 async def root():
